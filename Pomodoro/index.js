512804523d1a9c990f427f8ccc6a6590d64f9021
M Jean <maximejeanjgit@gmail.com>
2024-09-24 16:17:07 +0200

    initial commit

---
+++ Pomodoro/index.js
@@ -0,0 +1,11 @@
+function decompte(){
+    console.log("dddd");
+    let p_temps = document.getElementById("time");
+    p_temps.textContent = "25";
+    
+
+}
+
+
+let lancer = document.getElementById("lancer")
+//lancer.addEventListener('click',decompte(),false);

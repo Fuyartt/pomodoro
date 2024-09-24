87680289603fe1134f25d973e90b4b84d82b4093
M Jean <maximejeanjgit@gmail.com>
2024-09-24 16:58:29 +0200

    timer fonctionnel

--- Pomodoro/index.js
+++ Pomodoro/index.js
@@ -1,9 +1,47 @@
 function decompte(){
-    console.log("dddd");
-    let p_temps = document.getElementById("time");
-    p_temps.textContent = "25";
+    let compteur_sec = 59;
+    let compteur_min = 24
+    let p_min = document.getElementById("min");
+    let p_sec = document.getElementById("sec");
+    let interval = setInterval(deroule,1000);
+
+  /* p_sec.textContent = parseInt(p_sec.textContent) -1
+   if(parseInt(p_sec.textContent)<10){
+    if(parseInt(p_sec.textContent) == -1){
+        p_sec.textContent = 59;
+        p_min.textContent = parseInt(p_min.textContent) -1
+        if(parseInt(p_min.textContent) < 10){
+            p_min.textContent = 0 +p_min.textContent
+            if(parseInt(p_min.textContent) == 0){
+            }
+        }
+    } 
+    else{
+        p_sec.textContent = 0 +p_sec.textContent
+    }
+   }*/
     
 
+}
+
+function deroule(){
+    let p_min = document.getElementById("min");
+    let p_sec = document.getElementById("sec");
+    p_sec.textContent = parseInt(p_sec.textContent) -1
+    if(parseInt(p_sec.textContent)<10){
+     if(parseInt(p_sec.textContent) == -1){
+         p_sec.textContent = 59;
+         p_min.textContent = parseInt(p_min.textContent) -1
+         if(parseInt(p_min.textContent) < 10){
+             p_min.textContent = 0 +p_min.textContent
+             if(parseInt(p_min.textContent) == 0){
+             }
+         }
+     } 
+     else{
+         p_sec.textContent = 0 +p_sec.textContent
+     }
+}
 }
 
 

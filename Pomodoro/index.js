324ef213de5485efe5b12f4496b99e59f786944f
M Jean <maximejeanjgit@gmail.com>
2024-09-26 14:59:49 +0200

    le timer bascule entre travail et repos

--- Pomodoro/index.js
+++ Pomodoro/index.js
@@ -1,9 +1,23 @@
+let interval = null;
+let work_min = 24;
+let rest_min = 4;
+let work_or_rest = "work"
+
+function initialize(option){
+    if (option == true){
+        interval = setInterval(deroule,1000);
+    } else{
+        clearInterval(interval);
+    }
+}
+
 function decompte(){
+    document.getElementById("lancer").hidden = true;
     let compteur_sec = 59;
     let compteur_min = 24
     let p_min = document.getElementById("min");
     let p_sec = document.getElementById("sec");
-    let interval = setInterval(deroule,1000);
+    initialize(true);
 
   /* p_sec.textContent = parseInt(p_sec.textContent) -1
    if(parseInt(p_sec.textContent)<10){
@@ -33,9 +47,19 @@ function deroule(){
          p_sec.textContent = 59;
          p_min.textContent = parseInt(p_min.textContent) -1
          if(parseInt(p_min.textContent) < 10){
-             p_min.textContent = 0 +p_min.textContent
-             if(parseInt(p_min.textContent) == 0){
+            if(parseInt(p_min.textContent) == -1){
+                if(work_or_rest == "work"){
+                    p_min.textContent = rest_min;
+                    work_or_rest = "rest"
+                }else{
+                    p_min.textContent = work_min;
+                    work_or_rest = "work"
+                }
+             } else{
+                p_min.textContent = 0 +p_min.textContent
              }
+             
+             
          }
      } 
      else{

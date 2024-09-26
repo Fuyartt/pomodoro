09e47a3bdda38465238443d0f923e8c75b50e629
M Jean <maximejeanjgit@gmail.com>
2024-09-26 17:57:53 +0200

    ajout font size

--- index.js
+++ index.js
@@ -19,31 +19,10 @@ function reset(){
 
 function decompte(){
     document.getElementById("lancer").hidden = true;
-    let compteur_sec = 59;
-    let compteur_min = 24
-    let p_min = document.getElementById("min");
-    let p_sec = document.getElementById("sec");
     if(decompteur == true){
         initialize(true);
         decompteur = false;
     }
-
-  /* p_sec.textContent = parseInt(p_sec.textContent) -1
-   if(parseInt(p_sec.textContent)<10){
-    if(parseInt(p_sec.textContent) == -1){
-        p_sec.textContent = 59;
-        p_min.textContent = parseInt(p_min.textContent) -1
-        if(parseInt(p_min.textContent) < 10){
-            p_min.textContent = 0 +p_min.textContent
-            if(parseInt(p_min.textContent) == 0){
-            }
-        }
-    } 
-    else{
-        p_sec.textContent = 0 +p_sec.textContent
-    }
-   }*/
-    
 
 }
 
